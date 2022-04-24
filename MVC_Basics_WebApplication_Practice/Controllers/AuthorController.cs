257366cd3444551744237529f65aab813d208313
NILESH D <cricketlife224@gmail.com>
2022-04-24 09:24:54 +0530

    Add project files.

---
+++ MVC_Basics_WebApplication_Practice/Controllers/AuthorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using MVC_Basics_WebApplication_Practice.Models;
+
+namespace MVC_Basics_WebApplication_Practice.Controllers
+{
+    public class AuthorController : Controller
+    {
+        public IActionResult AuthorList()
+        {
+            List<Author> list = new List<Author>();
+            {
+                new Author { authorId=111,authorName="Nilesh",authorBookName="Public Administraction" };
+                new Author { authorId = 222, authorName = "Vikas", authorBookName = "Geography" };
+                new Author { authorId = 333, authorName = "Rahul", authorBookName = "Polity" };
+                new Author { authorId = 444, authorName = "vicky", authorBookName = "History" };
+            };
+            ViewBag.author = list;
+            return View();
+        }
+    }
+}
---
+++ MVC_Basics_WebApplication_Practice/Models/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_Basics_WebApplication_Practice.Models
+{
+    public class Author
+    {
+        public int authorId { get; set; }
+        public string authorName { get; set; }
+        public string authorBookName { get; set; }
+
+    }
+}
