f507b8b6f445ec973e67953a433e6e84eddd76a0
NILESH D <cricketlife224@gmail.com>
2022-04-24 10:12:01 +0530

    changes in AuthorController

--- MVC_Basics_WebApplication_Practice/Controllers/AuthorController.cs
+++ MVC_Basics_WebApplication_Practice/Controllers/AuthorController.cs
@@ -8,12 +8,12 @@ namespace MVC_Basics_WebApplication_Practice.Controllers
     {
         public IActionResult AuthorList()
         {
-            List<Author> list = new List<Author>();
+            List<Author> list = new List<Author>()
             {
-                new Author { authorId=111,authorName="Nilesh",authorBookName="Public Administraction" };
-                new Author { authorId = 222, authorName = "Vikas", authorBookName = "Geography" };
-                new Author { authorId = 333, authorName = "Rahul", authorBookName = "Polity" };
-                new Author { authorId = 444, authorName = "vicky", authorBookName = "History" };
+                new Author { authorId=111,authorName="Nilesh",authorBookName="Public Administraction" },
+                new Author { authorId = 222, authorName = "Vikas", authorBookName = "Geography" },
+                new Author { authorId = 333, authorName = "Rahul", authorBookName = "Polity" },
+                new Author { authorId = 444, authorName = "vicky", authorBookName = "History" },
             };
             ViewBag.author = list;
             return View();
